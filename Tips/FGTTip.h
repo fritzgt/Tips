36fc1fd0f72fc591d4bef34643f5497f4babec1f
exoticproductions <contactexoticproductions@gamail.com>
2021-06-30 11:50:48 +0300

    created properties

---
+++ Tips/FGTTip.h
@@ -0,0 +1,23 @@
+//
+//  FGTTip.h
+//  Tips
+//
+//  Created by FGT MAC on 6/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGTTip : NSObject
+
+
+@property (nonatomic) NSString *name;
+@property (nonatomic) double total;
+@property (nonatomic) NSInteger splitCount;
+@property (nonatomic) double tipPercentage;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tips/FGTTip.m
@@ -0,0 +1,17 @@
+//
+//  FGTTip.m
+//  Tips
+//
+//  Created by FGT MAC on 6/30/21.
+//
+
+#import "FGTTip.h"
+
+@implementation FGTTip
+
+
+
+
+
+
+@end
