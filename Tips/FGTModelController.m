2d833646b8bbb6260a5cb562020e9b347b5ae645
exoticproductions <contactexoticproductions@gamail.com>
2021-06-30 16:23:18 +0300

    implemented the getter for the tips array

--- Tips/FGTModelController.m
+++ Tips/FGTModelController.m
@@ -7,15 +7,30 @@
 
 #import "FGTModelController.h"
 
-//This is like a private H/declaration
+//This is like a private interface
 @interface FGTModelController (){
     NSMutableArray *_internalTips;
 }
 
 @end
 
+
+
 @implementation FGTModelController
 
+-(instancetype) init
+{
+    if (self = [super init]){
+        _internalTips = [[NSMutableArray alloc] init];
+    }
+    
+    return  self;
+}
+
+- (NSArray<FGTTip *> *)tips
+{
+    return _internalTips.copy;
+}
 
 
 @end
